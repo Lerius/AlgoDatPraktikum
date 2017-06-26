0b68a901fa77a81fe7b80568f02e8ed15a0cd5fc
ryancarmon <ryan.carmon@imaicarmordo.de>
2017-06-26 13:14:30 +0200

    Mögliche Lösung für Iteration

---
+++ IterativQS/QuicksortStackfree.java
@@ -0,0 +1,97 @@
+import java.util.Arrays;
+
+public class QuicksortStackfree {
+	public static void quicksort(int[] data, int start, int end) {
+		int pivot, left, right;
+		
+		while(start < end) {
+			pivot = data[end];
+			left = start;
+			right = end-1;
+			
+			while(true) {
+				while(data[left] < pivot && left < end) {
+					left++;
+				}
+				while(data[right] > pivot && right > start) {
+					right--;
+				}
+				
+				if(left < right) {
+					exchange(data, left, right);
+					left++;
+					right--;
+				} else {
+					break;
+				}
+			}					
+			
+			exchange(data, left, end);
+			
+			int startRight = left + 1;
+			
+			while(startRight < end) {
+				pivot = data[end];
+				left = startRight;
+				right = end-1;
+				
+				while(true) {
+					while(data[left] < pivot && left < end) {
+						left++;
+					}
+					while(data[right] > pivot && right > startRight) {
+						right--;
+					}
+					
+					if(left < right) {
+						exchange(data, left, right);
+						left++;
+						right--;
+					} else {
+						break;
+					}
+				}					
+				
+				exchange(data, left, end);
+				startRight = left+1;
+			}
+			end = left - 1;
+		}
+	}
+	
+	public static void exchange(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	public static void printArray(int[] array) {
+		System.out.println(Arrays.toString(array));
+	}
+	
+	public static void main(String[] args) {
+		int array [] = new int [50];
+		int worst [] = new int [] {1,2,3,4,5,6,7,8,9,10};
+		int best [] = new int [] {0, 4, 1, 3, 5, 6, 7, 2, 8};
+		int random [] = new int [1000];
+		int oberGrenze = 50; 
+		int oberGrenze2 = 1000;
+
+		for(int i = 0; i < array.length; i++) {
+		     array[i] = (int)(Math.random()*oberGrenze);
+		}
+	    printArray(array);
+
+		for(int i = 0; i < random.length; i++) {
+		     random[i] = (int)(Math.random()*oberGrenze2); 
+		}
+		
+		quicksort(array, 0, array.length-1);
+		quicksort(best, 0, best.length-1);
+		quicksort(random, 0, random.length-1);
+		quicksort(worst, 0, worst.length-1);
+		quicksort(best, 0, best.length-1);
+		
+		printArray(array);
+	}
+}
